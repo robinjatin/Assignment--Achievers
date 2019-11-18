38c729eb98793e83bcb4eda4203fccf3947b6a0a
Jatinvir Singh <jatinvirsingh4925@gmail.com>
2019-11-18 18:15:59 +0530

    java file created

---
+++ Arrayfreq.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Arrayfreq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		int max=arr[0];
+		for(int i=0;i<n;i++)
+		{
+			if(arr[i]>max)
+			{
+				max=arr[i];
+			}
+		}
+		int arr1[]=new int[max+1];
+		for(int i=0;i<n;i++)
+		{
+			arr1[arr[i]]++;
+		}
+		for(int i=0;i<=max;i++)
+		{
+			if(arr1[i]>0)
+			{
+				System.out.println(i+" occurs "+arr1[i]+" times");
+			}
+		}
+	}
+
+}
