4baae19f7f7c38a7d5de1d9c2b6cddd54c004847
Jatinvir Singh <jatinvirsingh4925@gmail.com>
2019-11-30 19:38:38 +0530

    Update LeftBT.java

--- LeftBT.java
+++ LeftBT.java
@@ -1,3 +1,5 @@
+/*Given a Binary Tree, print left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from left side
+*/
 import java.util.Scanner;
 class Node{
 	int data;
